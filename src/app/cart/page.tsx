de24756b84218eb0f15aa2ed5c5cc68f2a27c274
Ismail3427 <ismailurfy@gmail.com>
2026-07-06 12:44:32 -0500

    add cart page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,43 @@
+import { auth0 } from "@/lib/auth0";
+import axios from "axios";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Cart",
+  description: "This is the cart page",
+};
+
+const CartPage = async () => {
+  const session = await auth0.getSession();
+  const { data: cart } = await axios.post(
+    `${process.env.NEXT_PUBLIC_API_URL}/cart/get`,
+    {
+      userId: session?.user.sub,
+    },
+  );
+
+  return (
+    <main className="flex flex-col gap-5 items-center justify-center p-5">
+      <h1 className="font-bold text-6xl">Cart</h1>
+      <ul>
+        {cart.map((item: any) => (
+          <li
+            key={item.id}
+            className="shadow p-5 rounded-2xl hover:shadow-2xl transition-all duration-300 ease-in-out w-[50vw] flex justify-between"
+          >
+            <div className="flex flex-col gap-3">
+              <h2 className="font-bold text-2xl">{item.product.name}</h2>
+              <p>{item.product.description}</p>
+              <p>${(item.product.priceInCents / 100).toFixed(2)}</p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <p>Quantity: {item.quantity}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+export default CartPage;
--- src/components/add-to-cart-button/index.tsx
+++ src/components/add-to-cart-button/index.tsx
@@ -1,8 +1,28 @@
 "use client";
+import { useUser } from "@auth0/nextjs-auth0";
 import { Button } from "../ui/button";
+import axios from "axios";
+import { toast, Toaster } from "sonner";
 
 const AddToCartButton = () => {
-  return <Button>Add to Cart</Button>;
+  const { user } = useUser();
+
+  return (
+    <>
+      <Button
+        onClick={async () => {
+          await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/cart/add`, {
+            userId: user?.sub,
+            productId: window.location.pathname.split("/").pop(),
+          });
+          toast.success("Product added to cart!")
+        }}
+      >
+        Add to Cart
+      </Button>
+      <Toaster />
+    </>
+  );
 };
 
 export default AddToCartButton;
